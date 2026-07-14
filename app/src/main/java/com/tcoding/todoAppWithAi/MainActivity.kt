33962530fb93107709f5deaff679daee9d4011a3
Talha Sağlam <talhasaglam153@gmail.com>
2026-07-14 20:28:57 +0300

    Refactor MainActivity to use TaskAppContent and remove Greeting function

--- app/src/main/java/com/tcoding/todoAppWithAi/MainActivity.kt
+++ app/src/main/java/com/tcoding/todoAppWithAi/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.tcoding.todoAppWithAi.ui.task.TaskAppContent
 import com.tcoding.todoAppWithAi.ui.theme.TodoAppWithAiTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodoAppWithAiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Androidd",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TaskAppContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TodoPreview() {
+    TodoAppWithAiTheme {
+        TaskAppContent()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TodoAppWithAiTheme {
-        Greeting("Android")
-    }
-}
+fun GreetingPreview() = TodoPreview()
